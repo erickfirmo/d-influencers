5f647bd8d4b58f45b49a2d94fa5e912449de2987
Erick Firmo <erickfirmo1996@gmail.com>
2024-11-27 20:21:55 -0300

    feat: create influencers table

---
+++ app/Models/Influencer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Influencer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'instagram_username',
+        'followers_count',
+        'category',
+    ];
+}
