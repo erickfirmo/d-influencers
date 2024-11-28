0582cbbaa14f9665ccd62ac5ef37b30b9670c137
Erick Firmo <erickfirmo1996@gmail.com>
2024-11-27 22:21:05 -0300

    feat: implement JWT authentication

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::post('logout',  [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+    Route::get('influencers', function() {
+        dd('Test');
+    });
+
+});
